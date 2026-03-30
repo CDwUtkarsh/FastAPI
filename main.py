68e535edbc3f4efac9a052761f8c5acffdcad35f
Utkarsh Tiwari <utkarshtiwari8392@gmail.com>
2026-03-30 13:56:52 +0530

    LEarned some new concepts

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit=10,published:bool= True, sort:Optional[str] = None):
+    #only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blog from db'}
+    else:
+        return {'data': f'{limit} blog from db'}
 
 
 @app.get('/blog/unplublished')
@@ -19,5 +25,19 @@ def show(id:int):
 
 
 @app.get('/blog/{id}/comments')
-def comments(id:int):
+def comments(id,limit = 10):
+   
     return {'data': {'1', '2'}}
+
+
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    
+    return {'data': f'blog is created with title: {blog.title}'}
---
+++ practice.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+def index():
+    return {"data": "Welcome to FastAPI"}
+
+@app.get("/about")
+def about():
+    return {"data": "This is about page"}
+
+@app.get("/contact")
+def contact():
+    return {"data": "Contact us"}
+
+@app.get("/user")
+def get_user():
+    return {
+        "name": "Utkarsh",
+        "age": 21,
+        "Skills": ["Python", "FastAPI"]
+    }
+
+@app.get("/item/{item_id}")
+def get_item(item_id : int):
+    return {"item_id": item_id}
+
+@app.get("/hello/{name}")
+def get_name(name : str):
+    return {"name": f"hello {name}"}
+
+@app.get("/product/{id}")
+def get_product(id : int):
+    return {"id": id,"name": "Sample Product"}
+
+@app.get("/search")
+def search(q:str):
+    return {"query": q}
+
+
+@app.get("/filter")
+def filter(c :str,p:int):
+    return {"category": c,"price":p}
+
+# @app.get("/items")
+# def get_items(limit:int = 10):
+#     return {"limit":limit}
+
+
+items = [
+    {"id": 1, "name": "Laptop"},
+    {"id": 2, "name": "Phone"}
+]
+
+@app.get("/items/{id}")
+def get_item(id: int):
+    for item in items:
+        if item["id"] == id:
+            return item
+    return {"error": "Item not found"}
+
+  
